c721c547543e4dcc4a3a8b04aad1bd424330a0e9
IRABARUTA <willynorbert53@gmail.com>
2025-02-14 13:56:13 +0200

    g commit

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
+require('dotenv').config();
 const { Client } = require('pg');
 
-
 const client = new Client({
-  user: 'postgres',         
-  host: 'localhost',        
-  database: 'systemdatabase',       
-  password: '01402',  
-  port: 5432,                
+  user: process.env.PGUSER,         
+  host: process.env.PGHOST,        
+  database: process.env.PGDATABASE,       
+  password: process.env.PGPASSWORD,  
+  port: process.env.PGPORT,                
 });
-
 
 client.connect((err) => {
   if (err) {
@@ -18,4 +17,4 @@ client.connect((err) => {
   }
 });
 
-module.exports = client; 
+module.exports = client;
