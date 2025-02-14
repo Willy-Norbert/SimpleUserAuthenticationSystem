9b3644d051ba1521fea1ff1a9bfd6f1da22fde85
IRABARUTA <willynorbert53@gmail.com>
2025-02-14 16:15:10 +0200

    connected to db

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client({
-  user: process.env.PGUSER,         
-  host: process.env.PGHOST,        
-  database: process.env.PGDATABASE,       
-  password: process.env.PGPASSWORD,  
-  port: process.env.PGPORT,                
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: { rejectUnauthorized: false }  // Add this to enable SSL
 });
 
 client.connect((err) => {
